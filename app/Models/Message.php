5957d9ce9a73995d83e2600aa4c4f396e47da80f
Burhanuddin-Kachwala <burhanuddin.ast@gmail.com>
2025-05-06 13:53:57 +0530

    commented the morph code in the  Message Model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -12,13 +12,14 @@ class Message extends Model
         'sender_id', 'sender_type', 'receiver_id', 'receiver_type', 'message'
     ];
 
-    public function sender()
-    {
-        return $this->morphTo(__FUNCTION__, 'sender_type', 'sender_id');
-    }
+    // public function sender()
+    // {
+    //     dd($this->morphTo(__FUNCTION__, 'sender_type', 'sender_id'));
+    //     return $this->morphTo(__FUNCTION__, 'sender_type', 'sender_id');
+    // }
 
-    public function receiver()
-    {
-        return $this->morphTo(__FUNCTION__, 'receiver_type', 'receiver_id');
-    }
+    // public function receiver()
+    // {
+    //     return $this->morphTo(__FUNCTION__, 'receiver_type', 'receiver_id');
+    // }
 }
